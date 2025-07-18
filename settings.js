f66d398ab0d910d81d7a128296bf001b9dee4122
pchabot1234 <pcha8829@gmail.com>
2025-07-19 01:28:46 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Waleed Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botName: "Waleed Bot",
+  botOwner: 'WALEED', // Your name
   ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "YE BOT MENA SIRF NOOR KE LIYA BANAYA HA.",
   version: "2.0.6",
 };
 
